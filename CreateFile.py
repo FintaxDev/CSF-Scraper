32e671f306f702cf568b3979a2626ecbf25dfe8e
Yair Testas <65678421+yairtestas@users.noreply.github.com>
2022-06-27 22:19:35 -0500

    Corrección de errores

--- CreateFile.py
+++ CreateFile.py
@@ -13,15 +13,11 @@ def Json(url:str, filename:str)->None:
                 dicRows = {}
                 for Row in Table.children:
                     try:
-                        if Row["data-ri"] == "0":
-                            dicRows[Row.find("span").text] = Row.text.replace(Row.find("span").text, "")
-                        else:
-                            dicRows[Row.find("span").text] = Row.text.replace(Row.find("span").text, "")
+                        dicRows[Row.find("span").text] = Row.text.replace(Row.find("span").text, "")
                         dicTable[Table.parent.parent.parent.parent.parent.parent.parent.parent.parent.find("li").text] = dicRows
                     except:
                         pass
-            json_data = json.dumps(dicTable, indent=4, ensure_ascii=False)
             with open(filename, "w") as outfile:
-                json.dump(json_data, outfile, ensure_ascii=False)
+                json.dump(dicTable, outfile, indent=4, ensure_ascii=False)
     except requests.exceptions.ConnectionError as exc:
         print(exc)
