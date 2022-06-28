73a64c06d9366e1d9ffa86333b7fb8d441df13dd
Yair Testas <65678421+yairtestas@users.noreply.github.com>
2022-06-27 22:20:24 -0500

    Corrección de errores

--- ReadFile.py
+++ ReadFile.py
@@ -3,5 +3,4 @@ import json
 def Json(json_path:str)->dict:
     with open(json_path, "r") as openfile:
         json_data = json.load(openfile)
-    data = json.loads(json_data)
-    return data
+    return json_data
